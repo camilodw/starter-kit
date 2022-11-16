22d70cb50ada3e888467033beb9114b6c2cdabb1
Camnilo Oyarzo <camilo.oyarzodw@gmail.com>
2022-11-16 09:45:36 -0600

    cambio a español

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,13 +43,7 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">Iniciar sesión</a>
                                 </li>
                             @endif
                         @else
