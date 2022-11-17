95e6ea09d57f333943e8ee71b64608b430d9e532
Camnilo Oyarzo <camilo.oyarzodw@gmail.com>
2022-11-17 12:39:57 -0600

    lo pase a español

--- resources/views/auth/change-password.blade.php
+++ resources/views/auth/change-password.blade.php
@@ -5,25 +5,25 @@
             <div class="row">
                 <div class="col-lg-6 mx-auto">
                     <div class="card">
-                        <div class="card-header">Change Password</div>
+                        <div class="card-header">Cambiar contraseña</div>
                         <div class="card-body">
                             <form class="row g-3" method="post" action="{{route('update-password.store')}}">
                                 @csrf
                                 <div class="form-group col-md-6">
-                                    <label for="">Old Password</label>
+                                    <label for="">Contraseña actual</label>
                                     <input type="password" class="form-control" name="oldpass" required>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <label for="">New Password</label>
+                                    <label for="">Contraseña nueva</label>
                                     <input type="password" class="form-control" name="password" required>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <label for="">Current Password</label>
+                                    <label for="">Confirmar contraseña</label>
                                     <input type="password" class="form-control" name="password_confirmation" required>
                                 </div>
 
                                 <div class="col-12">
-                                    <button type="submit" class="btn btn-dark px-5">Save</button>
+                                    <button type="submit" class="btn btn-dark px-5">Guardar</button>
                                 </div>
                             </form>
                         </div>
